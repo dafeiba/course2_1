c9570ce006b9f34984dc3e996d9b9667fcd50b2f
黄飞 <2836734653@qq.com>
2017-12-27 14:01:47 +0800

    完成第二题第一个版本

---
+++ course2/src/version1/version1Controller.java
@@ -0,0 +1,88 @@
+package version1;
+
+import java.net.URL;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import version1.InfixInToSuffix;
+/*10*(8-6)+4*/
+public class version1Controller implements Initializable {
+	 ArrayList<Integer> list = new ArrayList<Integer>();
+	 InfixInToSuffix infixInToSuffix = new InfixInToSuffix();
+	 Alert alert = new Alert(AlertType.INFORMATION);
+	 
+	@FXML
+	private HBox hb;
+   @FXML
+   private Button myButton;
+   @FXML
+   private TextField tf;
+
+   @Override
+   public void initialize(URL location, ResourceBundle resources) {
+	   
+	   for (int i = 1; i <= 52; i++) {
+		      list.add(i);
+		    }
+	   
+	    java.util.Collections.shuffle(list);
+	    
+	    
+       // TODO (don't really need to do anything here).
+	    hb.getChildren().add(new ImageView("file:card/" + list.get(0) + ".png"));
+	    hb.getChildren().add(new ImageView("file:card/" + list.get(1) + ".png"));
+	    hb.getChildren().add(new ImageView("file:card/" + list.get(2) + ".png"));
+	    hb.getChildren().add(new ImageView("file:card/" + list.get(3) + ".png"));
+	   
+
+   }
+   
+@FXML 
+public void btn1(ActionEvent e){
+	hb.getChildren().clear();
+	 java.util.Collections.shuffle(list);
+	  hb.getChildren().add(new ImageView("file:card/" + list.get(0) + ".png"));
+	    hb.getChildren().add(new ImageView("file:card/" + list.get(1) + ".png"));
+	    hb.getChildren().add(new ImageView("file:card/" + list.get(2) + ".png"));
+	    hb.getChildren().add(new ImageView("file:card/" + list.get(3) + ".png"));
+}
+
+
+@FXML
+public void btn2(ActionEvent e) {
+	String text = tf.getText();
+	  String a = infixInToSuffix.toSuffix(text);//传入 一串 算数公式  
+	         
+	          if(infixInToSuffix.dealEquation(a).equals("24.0") ) {
+	        	  alert.setContentText("correct");
+	          }
+	          
+	          else
+	        	  alert.setContentText("incorrectNumbers");
+	        	  
+	           alert.show();
+	           
+	           
+	           
+	
+	
+}
+
+
+}
